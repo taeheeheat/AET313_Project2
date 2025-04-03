5e653a1cbd1a11a2ee60b50c892b006182ea1473
taeheeheat <thkim539@gmail.com>
2025-04-03 01:38:30 -0500

    Add files via upload

---
+++ keileyProject2.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo servo1; //create servo variable
+const int buttonPin = 2;
+
+void setup() {
+  servo1.attach(9); // attaches the servo on pin 9
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == HIGH {
+    servo1.write(180); //tells the servo what angle to turn to 0-180
+  } else {
+    servo1.write(0);
+  }
+
+}

df6b3115fb7abcb9ac7bce89882c22a6b093123c
taeheeheat <thkim539@gmail.com>
2025-04-11 17:01:28 -0500

    Update keileyProject2.ino

--- keileyProject2.ino
+++ keileyProject2.ino
@@ -1,17 +1,67 @@
-#include <Servo.h> //changes test! 
+#include <Servo.h>
 
-Servo servo1; //create servo variable
-const int buttonPin = 2; //sets pin to 2
+const int NUM_HOUSES = 3;
+
+// servo
+Servo houseServos[NUM_HOUSES];
+Servo wolfServo;
+
+// servo pin
+const int housePins[NUM_HOUSES] = {8, 9, 10}; //8-straw, 9-wood, 10-brick
+const int wolfPin = 11;
+
+// btn pin
+const int pigBtns[NUM_HOUSES] = {2, 3, 4}; //2-straw, 3-wood, 4-brick
+//마지막 led 버튼 추가하기
+const int wolfBtns[NUM_HOUSES] = {5, 6, 7}; //5-straw, 6-wood, 7-brick
+
+// LED pin
+const int ledPin = 13;
+
+// state check
+bool houseOpen[NUM_HOUSES] = {false, false, false};
+}
 
 void setup() {
-  servo1.attach(9); // attaches the servo on pin 9
-  pinMode(buttonPin, INPUT);
+  for (int i = 0; i < NUM_HOUSES; i++) {
+    houseServos[i].attach(housePins[i]);
+    pinMode(pigBtns[i], INPUT);
+    pinMode(wolfBtns[i], INPUT);
+  }
+
+  wolfServo.attach(wolfPin);
+  pinMode(resetBtn, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  if (digitalRead(buttonPin) == HIGH {
-    servo1.write(90); //the servo turn to 90 degree
-  } else {
-    servo1.write(0);
+  // Each pig makes each houses' servo turns 180 degree
+  for (int i = 0; i < NUM_HOUSES; i++) {
+    if (digitalRead(pigBtns[i]) == HIGH && !houseOpen[i]) {
+      houseServos[i].write(180);
+      houseOpen[i] = true;
+
+      // if brickPigBtn activates --> wolf servo give a wolf pieces
+      if (i == 2) {
+        wolfServo.write(90);
+      }
+
+      delay(200);
+    }
+  }
+
+  // Wolf makes each houses' servo turns 0 degree
+  for (int i = 0; i < NUM_HOUSES; i++) {
+    if (digitalRead(wolfBtns[i]) == HIGH && houseOpen[i]) {
+      houseServos[i].write(0);
+      houseOpen[i] = false;
+
+      //cake led
+      if (i == 2) {
+        //코드 추가하기
+      }
+
+      delay(200);
+    }
   }
 }
